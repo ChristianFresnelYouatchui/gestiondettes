94ac3146df930dff6d815f0b126bdaef50818345
ChristianFresnelYouatchui <christian-fresnel.youatchui@ism.edu.sn>
2025-06-19 20:50:31 +0000

    mise a jour ok

--- gestiondettes/src/main/java/com/example/gestiondettes/repository/DetteRepository.java
+++ gestiondettes/src/main/java/com/example/gestiondettes/repository/DetteRepository.java
@@ -12,13 +12,3 @@ public interface DetteRepository extends JpaRepository<Dette, Long> {
 }
 
 
-
-
-// import com.example.gestiondettes.entity.Dette;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import java.util.List;
-
-// public interface DetteRepository extends JpaRepository<Dette, Long> {
-//     List<Dette> findByClientId(Long clientId);
-// }
--- gestiondettes/src/main/java/com/example/gestiondettes/service/DetteService.java
+++ gestiondettes/src/main/java/com/example/gestiondettes/service/DetteService.java
@@ -37,3 +37,4 @@ public class DetteService {
         return detteRepository.findById(id);
     }
 }
+
